ef13ab990da985640a5ba0484e9e15001aa2bb7b
skinmdc <57782887+skinmdc@users.noreply.github.com>
2019-11-15 12:53:00 +0800

    Add files via upload

---
+++ buttonhandler.php
@@ -0,0 +1,182 @@
+<?php
+@ini_set("display_errors","1");
+@ini_set("display_startup_errors","1");
+
+require_once("include/dbcommon.php");
+require_once("classes/button.php");
+
+$params = (array)my_json_decode(postvalue('params'));
+
+if( $params["_base64fields"] ) {
+	foreach( $params["_base64fields"] as $f ) {
+		$params[$f] = base64_decode( $params[$f] );
+	}
+}
+
+$buttId = $params['buttId'];
+$eventId = postvalue('event');
+$table = $params['table'];
+if( !GetTableURL( $table ) ) {
+	exit;
+}
+$page = $params['page'];
+if( !Security::userCanSeePage($table, $page ) ) {
+	exit;
+}
+
+$pSet = new ProjectSettings( $table, "", $page );
+if( $buttId ) {
+	$pageButtons = $pSet->customButtons();
+	if( array_search( $buttId , $pageButtons ) === false ) {
+		exit;
+	}
+}
+
+if($buttId=='Deactivate')
+{
+	//  for login page users table can be turned off
+	if( $table != GLOBAL_PAGES )
+	{
+		require_once("include/". GetTableURL( $table ) ."_variables.php");
+		$cipherer = new RunnerCipherer( $table );
+	}
+	buttonHandler_Deactivate($params);
+}
+if($buttId=='Deactivate_User')
+{
+	//  for login page users table can be turned off
+	if( $table != GLOBAL_PAGES )
+	{
+		require_once("include/". GetTableURL( $table ) ."_variables.php");
+		$cipherer = new RunnerCipherer( $table );
+	}
+	buttonHandler_Deactivate_User($params);
+}
+
+
+
+
+
+// create table and non table handlers
+function buttonHandler_Deactivate($params)
+{
+	global $strTableName;
+	$result = array();
+
+	// create new button object for get record data
+	$params["keys"] = (array)my_json_decode(postvalue('keys'));
+	$params["isManyKeys"] = postvalue('isManyKeys');
+	$params["location"] = postvalue('location');
+
+	$button = new Button($params);
+	$ajax = $button; // for examle from HELP
+	$keys = $button->getKeys();
+
+	$masterData = false;
+	if ( isset($params['masterData']) && count($params['masterData']) > 0 )
+	{
+		$masterData = $params['masterData'];
+	}
+	else if ( isset($params["masterTable"]) )
+	{
+		$masterData = $button->getMasterData($params["masterTable"]);
+	}
+	
+	$contextParams = array();
+	if ( $params["location"] == PAGE_VIEW )
+	{
+		$contextParams["data"] = $button->getRecordData();
+		$contextParams["masterData"] = $masterData;
+	}
+	else if ( $params["location"] == PAGE_EDIT )
+	{
+		$contextParams["data"] = $button->getRecordData();
+		$contextParams["newData"] = $params['fieldsData'];
+		$contextParams["masterData"] = $masterData;
+	}
+	else if ( $params["location"] == "grid" )
+	{	
+		$params["location"] = "list";
+		$contextParams["data"] = $button->getRecordData();
+		$contextParams["newData"] = $params['fieldsData'];
+		$contextParams["masterData"] = $masterData;
+	}
+	else 
+	{
+		$contextParams["masterData"] = $masterData;
+	}
+
+	RunnerContext::push( new RunnerContextItem( $params["location"], $contextParams));
+	global $dal;
+ 
+while ( $data = $button->getNextSelectedRecord() ) {
+  // set ReportsTo field to 'Bob Smith'
+  $sql = "Update users set access_type='Deactivated' where user_id=".$data['user_id'];
+   CustomQuery($sql);
+}
+$result["txt"] = "User(s) Deactivated.";;
+	RunnerContext::pop();
+	echo my_json_encode($result);
+	$button->deleteTempFiles();
+}
+function buttonHandler_Deactivate_User($params)
+{
+	global $strTableName;
+	$result = array();
+
+	// create new button object for get record data
+	$params["keys"] = (array)my_json_decode(postvalue('keys'));
+	$params["isManyKeys"] = postvalue('isManyKeys');
+	$params["location"] = postvalue('location');
+
+	$button = new Button($params);
+	$ajax = $button; // for examle from HELP
+	$keys = $button->getKeys();
+
+	$masterData = false;
+	if ( isset($params['masterData']) && count($params['masterData']) > 0 )
+	{
+		$masterData = $params['masterData'];
+	}
+	else if ( isset($params["masterTable"]) )
+	{
+		$masterData = $button->getMasterData($params["masterTable"]);
+	}
+	
+	$contextParams = array();
+	if ( $params["location"] == PAGE_VIEW )
+	{
+		$contextParams["data"] = $button->getRecordData();
+		$contextParams["masterData"] = $masterData;
+	}
+	else if ( $params["location"] == PAGE_EDIT )
+	{
+		$contextParams["data"] = $button->getRecordData();
+		$contextParams["newData"] = $params['fieldsData'];
+		$contextParams["masterData"] = $masterData;
+	}
+	else if ( $params["location"] == "grid" )
+	{	
+		$params["location"] = "list";
+		$contextParams["data"] = $button->getRecordData();
+		$contextParams["newData"] = $params['fieldsData'];
+		$contextParams["masterData"] = $masterData;
+	}
+	else 
+	{
+		$contextParams["masterData"] = $masterData;
+	}
+
+	RunnerContext::push( new RunnerContextItem( $params["location"], $contextParams));
+	$record = $button->getCurrentRecord();
+
+$sql = "Update users set access_type='Deactivated' where user_id=".$record['user_id'];
+   CustomQuery($sql);;
+	RunnerContext::pop();
+	echo my_json_encode($result);
+	$button->deleteTempFiles();
+}
+
+
+		
+?>
